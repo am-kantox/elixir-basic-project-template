6f7171eeaf28da219b98d05019b32710d7d1d591
Georgiy Bykov <zhora.bykov@gmail.com>
2021-12-13 14:44:47 +0300

    GitHub Actions for Elixir project with fixed caching (#2)

--- .dialyzer-ignore.exs
+++ .dialyzer-ignore.exs
@@ -1,5 +1,3 @@
-# The list of the files to ignoge could be placed below.
+# The list of the warnings to ignore could be placed below.
 #   SEE: https://github.com/jeremyjh/dialyxir#elixir-term-format
-[
-  {"lib/kv_behaviour.ex", :unknown_type}
-]
+[]
